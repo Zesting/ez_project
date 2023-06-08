5c866915716796d9141b8635a6ede881d5de2266
Dooruna <goldtwin33@naver.com>
2023-06-08 22:02:59 +0900

    재영 커밋

--- src/main/java/Ezen/project/SpringConfig.java
+++ src/main/java/Ezen/project/SpringConfig.java
@@ -4,14 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import Ezen.project.repository.JpaPaymentRepository;
 import Ezen.project.repository.JpaRoomRepository;
 import Ezen.project.repository.JpaSampleRepository;
-import Ezen.project.repository.PaymentRepository;
 import Ezen.project.repository.RoomRepository;
 import Ezen.project.repository.SampleRepository;
 import Ezen.project.service.RoomService;
-import Ezen.project.service.PaymentService;
 import Ezen.project.service.SampleService;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
@@ -43,13 +40,4 @@ public class SpringConfig {
         return new JpaRoomRepository(em);
     }
 
-    @Bean
-    public PaymentService paymentsService() {
-        return new PaymentService(paymentRepository());
-    }
-
-    @Bean
-    public PaymentRepository paymentRepository() {
-        return new JpaPaymentRepository(em);
-    }
 }
--- src/main/java/Ezen/project/service/PaymentService.java
+++ src/main/java/Ezen/project/service/PaymentService.java
@@ -3,6 +3,7 @@ package Ezen.project.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import Ezen.project.domain.Payment;
@@ -10,35 +11,38 @@ import Ezen.project.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 
 @Transactional
+@Service
 @RequiredArgsConstructor
 public class PaymentService {
 
   private final PaymentRepository paymentRepository;
 
+  //결제 정보 저장
   public Payment save(Payment payment) {
     paymentRepository.save(payment);
     return payment;
   }
   
+  //전체 결제 리스트 출력
   public List<Payment> findByAll() {
-    return paymentRepository.findByAll();
+    System.out.println("전체 결제 리스트 출력");
+    return paymentRepository.findAll();
   }
 
   public Optional<Payment> findById(Long payId) {
-  return null;
-
-  }
-
-  public Optional<Payment> findByName(String userId) {
     return null;
-  }
-
-  public Optional<Payment> update(Payment payment) {
-    return null;
-  }
-
-  public Long delete(Long payId) {
-    return null;
-    
-  }
+    }
+  
+    public Optional<Payment> findByName(String userId) {
+      return null;
+    }
+  
+    public Optional<Payment> update(Payment payment) {
+      return null;
+    }
+  
+    public Long delete(Long payId) {
+      return null;
+      
+    }
 }
