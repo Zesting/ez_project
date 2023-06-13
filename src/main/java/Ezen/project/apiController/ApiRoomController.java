a7ce7aab60681818a6b89a661aec83948a1d3ed6
Zesting <pp950209@naver.com>
2023-06-13 12:49:23 +0900

    admin_loomList_commit_박준희

--- src/main/java/Ezen/project/apiController/ApiRoomController.java
+++ src/main/java/Ezen/project/apiController/ApiRoomController.java
@@ -1,14 +1,17 @@
 package Ezen.project.apiController;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import Ezen.project.DTO.RoomDTO;
@@ -27,6 +30,11 @@ public class ApiRoomController {
     public ResponseEntity<Room> add(@RequestBody RoomDTO roomDTO) {
         Optional<Room> result = roomService.roomJoin(roomDTO);
         return ResponseEntity.ok(result.get());
+    }
+
+    @GetMapping(value = "")
+    public List<Room> roomList() {
+        return roomService.findAllRoom();
     }
 
     @PutMapping(value = "/modify/{id}")
