f1a8a17a7b159661d8164a898e86dcd513c2c8af
Dooruna <goldtwin33@naver.com>
2023-06-14 09:18:01 +0900

    0614 재영 커밋_카카오맵 추가 및 결제데이터 수정

--- src/main/java/Ezen/project/controller/paymentController.java
+++ src/main/java/Ezen/project/controller/paymentController.java
@@ -2,7 +2,6 @@ package Ezen.project.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +19,6 @@ import Ezen.project.service.RoomService;
 import Ezen.project.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.java.Log;
 
 @Log
@@ -29,40 +27,41 @@ import lombok.extern.java.Log;
 public class PaymentController {
   private final PaymentService paymentService;
   private final UserService userService;
-  @Setter(onMethod_ = @Autowired)
-  private KakaopayService kakaoPayService;
+  private final KakaopayService kakaoPayService;
   private final RoomService roomService;
 
-  @GetMapping("/kakaoPay")
-  public void kakaoPayServiceGet() {
-
-  }
-
+  //결제하기 버튼 눌렀을때.
   @PostMapping("/kakaoPay")
   public String kakaoPayService(HttpSession session, RoomDTO roomDTO) {
     log.info("kakaoPayService post.........................................");
+    //세션 아이디가 없으면 로그인창으로 보내기
+    if(session.getAttribute("userId") == null){
+      return "redirect:/login";
+    } 
     //세션 아이디 가져옴.
     Long userId = (Long) session.getAttribute("userId");
-    Reservation reservation = (Reservation) session.getAttribute("reservation");
+    //예약 정보 reservationController에서 세션에 저장하여 가져옴.
+    if(session.getAttribute("payReservation")==null){
+      return "redirect:/reservation/selectDate";      
+    }
+    Reservation reservation = (Reservation) session.getAttribute("payReservation");
+    //로그인 되어 있는 userID를 가져옴
     UserDTO logInUser = userService.findById(userId);
-
+    //새로운 객체를 만들어 필요한 값을 저장.
     PaymentDTO paymentDTO = new PaymentDTO();
     paymentDTO.setReservationId(reservation.getReservationId());
-    paymentDTO.setUserName(logInUser.getUserId());  //들어온것 확인함.
-    paymentDTO.setRoomName(roomService.findRoomById(reservation.getRoomId()).get().getRoomName());//
+    paymentDTO.setUserName(logInUser.getUserId());
+    //룸 ID로 찾아 예약된 룸아이디의 룸이름을 가져옴
+    paymentDTO.setRoomName(roomService.findRoomById(reservation.getRoomId()).get().getRoomName());
     paymentDTO.setAmount(reservation.getFinalPrice());
+    //다른 곳에서 DTO를 쓰기 위해 세션에 paymentDTO를 저장.
     session.setAttribute("paymentDTO", paymentDTO);
-    //셋팅해서 넣어야할 값들
-  // private Long id;
-  // private Long reservationId;//예약 번호
-  // private String userName;    //회원 이름
-  // private String rommName;    //룸 이름
-  // private int amount;    //가격
+    
   System.out.println("포스트맵핑/kakaopay dto 저장"+paymentDTO);
     return "redirect:" + kakaoPayService.kakaoPayReady(paymentDTO);
   }
 
-  // 결제가 성공되었음.
+  // 결제가 성공되었을때.
   @GetMapping("/kakaoPaySuccess")
   public String kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model, HttpSession session) {
     log.info("kakaoPaySuccess get............................................");
@@ -73,10 +72,11 @@ public class PaymentController {
     return "payment/kakaoPaySuccess";
   }
 
+  //결제 진행 중 취소하였을 때.
   @GetMapping("/kakaoPayCancel")
-  // public String kakaoPayCancel(@RequestParam("amount") int
-  // amount,@RequestParam("tid") String tid,Model model) {
   public String kakaoPayCancel() {
+    //취소 되었다고 뷰로 표시
+    //예약에 마지막 저장된 데이터를 삭제해야함. >> 준희가 결제 여부 만들어서 올려준다함.
 
     return "payment/kakaoPayCancel";
   }
@@ -88,16 +88,11 @@ public class PaymentController {
 
   // 아래는 카카오 페이랑 관련 x 다른 api및 테스트하던 매핑.
 
+  //결제 버튼 있는 페이지
   @GetMapping("/payment")
   public String payment() {
     return "payment/payment";
   }
-
-  // @PostMapping("/payment")
-  // public String kakaoPay() {
-
-  //   return null;
-  // }
 
   // Payment테이블에 결제 리스트 조회
   @GetMapping("/paymentList")
