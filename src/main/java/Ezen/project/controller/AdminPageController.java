2e6f3fcdc4021c5087c1dda0eafeeed6380cd705
Dooruna <goldtwin33@naver.com>
2023-06-26 17:04:24 +0900

    0626 재영 커밋_관리자 홈_카운트 수정

--- src/main/java/Ezen/project/controller/AdminPageController.java
+++ src/main/java/Ezen/project/controller/AdminPageController.java
@@ -1,5 +1,7 @@
 package Ezen.project.controller;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -7,13 +9,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import Ezen.project.DTO.AdminCountDTO;
 import Ezen.project.DTO.NoticeDTO;
 import Ezen.project.DTO.PaymentInfoDTO;
 import Ezen.project.DTO.UserDTO;
 import Ezen.project.DTO.WeddingDTO;
 import Ezen.project.domain.Payment;
+import Ezen.project.domain.Reservation;
+import Ezen.project.domain.Room;
 import Ezen.project.service.NoticeService;
 import Ezen.project.service.PaymentService;
+import Ezen.project.service.ReservationService;
+import Ezen.project.service.RoomService;
 import Ezen.project.service.UserService;
 import Ezen.project.service.WeddingService;
 import jakarta.servlet.http.HttpSession;
@@ -29,24 +36,63 @@ public class AdminPageController {
   private final NoticeService noticeService;
   private final WeddingService weddingService;
   private final PaymentService paymentService;
+  private final ReservationService reservationService;
+  private final RoomService roomService;
 
   @GetMapping("/adminPage")
-  public String adminPage(Model model, HttpSession session){
-    if(session.getAttribute("userId") == null) {
+  public String adminPage(Model model, HttpSession session) {
+    if (session.getAttribute("userId") == null) {
       return "redirect:/login";
     }
     List<UserDTO> userDTOList = userService.findAll();
     model.addAttribute("userList", userDTOList);
+    Date today = new Date(System.currentTimeMillis());
+    List<Reservation> checkInRoom = reservationService.findAllReservationByCheckIn(today);
+    List<Room> allRoom = roomService.findAllRoom();
+
+    System.out.println("allRoom -> " + allRoom);
+    System.out.println("오늘 체크인 된 수:" + checkInRoom);
+    List<String> nameList = new ArrayList<>();
+    for (int i = 0; i < checkInRoom.size(); i++) {
+      // System.out.println(checkInRoom.get(i).getRoomId()); //룸 ID 확인
+      Room roomName = roomService.findRoomById(checkInRoom.get(i).getRoomId()).get();
+      System.out.println(roomName.getRoomName()); // 룸 이름 확인
+      nameList.add(roomName.getRoomName());
+    }
+    ;
+    System.out.println(nameList);
+    int earthCount = 0, marsCount = 0, jupiterCount = 0, mercuryCount = 0;
+    for (int i = 0; i < nameList.size(); i++) {
+      if (nameList.get(i).contains("Mercury")) {
+        mercuryCount++;
+      } else if (nameList.get(i).contains("Mars")) {
+        marsCount++;
+      } else if (nameList.get(i).contains("Earth")) {
+        earthCount++;
+      } else if (nameList.get(i).contains("Jupiter")) {
+        jupiterCount++;
+      }
+    }
+    System.out.println("Mercury 카운트: " + mercuryCount);
+    System.out.println("Earth 카운트: " + earthCount);
+    System.out.println("Mars 카운트: " + marsCount);
+    System.out.println("Jupiter 카운트: " + jupiterCount);
+    AdminCountDTO adminCountDTO = new AdminCountDTO();
+    adminCountDTO.setMercuryCount(mercuryCount);
+    adminCountDTO.setMarsCount(marsCount);
+    adminCountDTO.setEarthCount(earthCount);
+    adminCountDTO.setJupiterCount(jupiterCount);
+    model.addAttribute("roomNameCount", adminCountDTO);
     return "admin/adminPage";
   }
 
   @GetMapping("/adminPageSave")
-  public String saveForm(){
-    
-    return "admin/adminPageSave";
-    }
+  public String saveForm() {
 
-  //결제된 정보 리스트
+    return "admin/adminPageSave";
+  }
+
+  // 결제된 정보 리스트
   @GetMapping("/adminPaymentInfo/{id}")
   public String paymentInfo(HttpSession session, Model model, @PathVariable("id") Long payId) {
     Long userDTO = (Long) session.getAttribute("userId");
@@ -78,69 +124,69 @@ public class AdminPageController {
 
   // 공지 노페이징 맵핑
   @GetMapping("/noticeList")
-  public String noticeList(Model model){
-        List<NoticeDTO> noticeDTOList = noticeService.findAll();
-        model.addAttribute("noticeList", noticeDTOList);
+  public String noticeList(Model model) {
+    List<NoticeDTO> noticeDTOList = noticeService.findAll();
+    model.addAttribute("noticeList", noticeDTOList);
     return "/notice/adminNoticeList";
   }
 
   // 공지 페이징 매핑
   // @GetMapping("/noticeList")
-  // public String noticeList(@PageableDefault(page = 1) Pageable pageable, Model model){
-  //       // pageable.getPageNumber();
-  //       Page<NoticeDTO> noticeList = noticeService.paging(pageable);
-  //       int blockLimit = 5;
-  //       int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
-  //       int endPage = ((startPage + blockLimit - 1) < noticeList.getTotalPages()) ? startPage + blockLimit - 1 : noticeList.getTotalPages();
+  // public String noticeList(@PageableDefault(page = 1) Pageable pageable, Model
+  // model){
+  // // pageable.getPageNumber();
+  // Page<NoticeDTO> noticeList = noticeService.paging(pageable);
+  // int blockLimit = 5;
+  // int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() /
+  // blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+  // int endPage = ((startPage + blockLimit - 1) < noticeList.getTotalPages()) ?
+  // startPage + blockLimit - 1 : noticeList.getTotalPages();
 
-  //       model.addAttribute("noticeList", noticeList);
-  //       model.addAttribute("startPage", startPage);
-  //       model.addAttribute("endPage", endPage);
-  //   return "/notice/adminNoticeListPaging";
+  // model.addAttribute("noticeList", noticeList);
+  // model.addAttribute("startPage", startPage);
+  // model.addAttribute("endPage", endPage);
+  // return "/notice/adminNoticeListPaging";
   // }
 
-  //게시글 삭제
-    @GetMapping("/adminNotice/delete/{id}")
-    public String delete(@PathVariable Long id){
-        noticeService.delete(id);
-        return "redirect:/adminPage";
-    }
+  // 게시글 삭제
+  @GetMapping("/adminNotice/delete/{id}")
+  public String delete(@PathVariable Long id) {
+    noticeService.delete(id);
+    return "redirect:/adminPage";
+  }
 
+  // 웨딩 리스트 조회
+  @GetMapping("/weddingList")
+  public String weddingList(Model model) {
+    List<WeddingDTO> weddingDTOList = weddingService.findAll();
+    model.addAttribute("weddingList", weddingDTOList);
+    return "/wedding/adminWeddingList";
+  }
 
-  //웨딩 리스트 조회
-    @GetMapping("/weddingList")
-    public String weddingList(Model model){
-       List<WeddingDTO> weddingDTOList = weddingService.findAll();
-       model.addAttribute("weddingList", weddingDTOList);
-        return "/wedding/adminWeddingList";
-    }
-
-  
-
-    // 웨딩 문의 페이징 매핑
+  // 웨딩 문의 페이징 매핑
   // @GetMapping("/weddingList")
-  //   public String paging(@PageableDefault(page = 1) Pageable pageable, Model model){
-  //       Page<WeddingDTO> weddingList = weddingService.paging(pageable);
-  //       int blockLimit = 5;
-  //       int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
-  //       int endPage = ((startPage + blockLimit - 1) < weddingList.getTotalPages()) ? startPage + blockLimit - 1 : weddingList.getTotalPages();
+  // public String paging(@PageableDefault(page = 1) Pageable pageable, Model
+  // model){
+  // Page<WeddingDTO> weddingList = weddingService.paging(pageable);
+  // int blockLimit = 5;
+  // int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() /
+  // blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+  // int endPage = ((startPage + blockLimit - 1) < weddingList.getTotalPages()) ?
+  // startPage + blockLimit - 1 : weddingList.getTotalPages();
 
-  //       model.addAttribute("weddingList", weddingList);
-  //       model.addAttribute("startPage", startPage);
-  //       model.addAttribute("endPage", endPage);
+  // model.addAttribute("weddingList", weddingList);
+  // model.addAttribute("startPage", startPage);
+  // model.addAttribute("endPage", endPage);
 
-  //       return "/wedding/adminWeddingPaging";
-  //   }
+  // return "/wedding/adminWeddingPaging";
+  // }
 
-
-  //공지사항 등록
+  // 공지사항 등록
   @GetMapping("/adminNoticeSave")
-    public String adminSave(Model model, HttpSession session){
-       Long userId = (Long) session.getAttribute("userId");
-        model.addAttribute("user", userService.findById(userId));
-        return "/notice/adminNoticeSave";
-    }
+  public String adminSave(Model model, HttpSession session) {
+    Long userId = (Long) session.getAttribute("userId");
+    model.addAttribute("user", userService.findById(userId));
+    return "/notice/adminNoticeSave";
+  }
 
 }
-
- 
---
+++ src/main/java/Ezen/project/dto/AdminCountDTO.java
@@ -0,0 +1,11 @@
+package Ezen.project.DTO;
+
+import lombok.Data;
+
+@Data
+public class AdminCountDTO {
+  private int mercuryCount;
+  private int marsCount;
+  private int earthCount;
+  private int jupiterCount;
+}
